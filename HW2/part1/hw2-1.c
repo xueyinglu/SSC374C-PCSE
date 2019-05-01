97705dc68e94862e082fa12461a85076e27c1481
Xueying <xylu@utexas.edu>
2019-05-01 11:23:22 -0500

    HW2 part1

--- HW2/part1/hw2-1.c
+++ HW2/part1/hw2-1.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include <omp.h>
+#include <math.h>
+#include <mpi.h>
 void initialize(float **x,int n){
     srand(time(NULL));
     for (int i= 0; i<n; i++)
@@ -11,44 +12,77 @@ void initialize(float **x,int n){
     }
 }
 
-void smooth(float **x,float **y,int n,float a,float b,float c){
-    int i;
-#pragma omp parallel 
-{
-#pragma omp for schedule(static)
-    for (i= 1; i<n-1; i++){
+void smooth(float **x,float **y,int n,float a,float b,float c, int myid, int dim, int numprocs){
+    /* define a column type*/
+    MPI_Datatype column;
+    MPI_Type_vector(n,1,n,MPI_FLOAT,&column);
+    MPI_Type_commit(&column);
+
+    /* send and receive row vectors*/
+    if (myid<numprocs-dim){
+    // send the bottom row to the adjacent processor at the bottom
+        MPI_Send(&x[n-2][0],n,MPI_FLOAT,myid+dim,myid,MPI_COMM_WORLD);
+    }
+    if (myid>=dim){
+        // receive the bottom row from the adjacent processor on the top
+        MPI_Recv(&x[0][0],n,MPI_FLOAT,myid-dim,myid-dim,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        // send the top row to the adjacent processor on the top
+        MPI_Send(&x[1][0],n,MPI_FLOAT,myid-dim,myid,MPI_COMM_WORLD);
+        
+    }
+    if (myid<numprocs-dim){
+        // receive the top row from the adjacent processor at the bottom
+        MPI_Recv(&x[n-1][0],n,MPI_FLOAT,myid+dim, myid+dim, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    }
+    
+    /* send and receive the column vectors*/
+
+    if ((myid+1)%dim !=0){
+        // send the right column to the adjacent processor on the right
+        MPI_Send(&x[n-2][0],1,column,myid+1,myid,MPI_COMM_WORLD);
+
+    }
+    if (myid%dim !=0){
+        // receive from the left
+        MPI_Recv(&x[0][0],1,column,myid-1,myid-1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        // send the left column to the adjacent processor on the left
+        MPI_Send(&x[1][0],1,column,myid-1,myid,MPI_COMM_WORLD);
+    }
+    if ((myid+1)%dim !=0){
+        // receive from the right
+        MPI_Recv(&x[n-1][0],1,column,myid+1,myid+1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+    }
+    for (int i= 1; i<n-1; i++){
+
         int j;
-        //printf("This is thread %d, i=%d\n", omp_get_thread_num(), i);
         for (j= 1; j<n-1; j++){
             y[i][j] = a*(x[i-1][j-1] + x[i-1][j+1] + x[i+1][j-1] + x[i+1][j+1]) 
                         + b*(x[i-1][j+0] + x[i+1][j+0] + x[i+0][j-1] + x[i+0][j+1]) 
                         + c* x[i+0][j+0];
         }
-    }
 }
 }
 
 void count(float **y,int n,float t,int *ct){
 int localct=*ct;
 int i,j;
-#pragma omp parallel
-{
-#pragma omp for reduction(+:localct) private(i,j)
     for (i= 1; i<n-1; i++){
     for (j= 0; j<n-1; j++){
         if (y[i][j]<t) {localct+=1;}
     }
 }
-}
 *ct=localct;
 }
 
-int main(){
-#pragma omp parallel
-{
-printf("This is thread %d\n", omp_get_thread_num());
-}
-    long n = 98306;
+int main( int argc, char*argv[]){
+
+    int numprocs, myid;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    int dim = (int)(sqrt(numprocs)); // number of processors in each direction.
+    int n = 16384;
 
 //    int n=20;
     float a = 0.05;
@@ -80,24 +114,12 @@ printf("This is thread %d\n", omp_get_thread_num());
     }
     float a4=clock();
     float t2= (a4 - a3)/(float)CLOCKS_PER_SEC;
-    double i1=omp_get_wtime();
     initialize(x,n);
-    double i2=omp_get_wtime();
-    double t3= (i2 - i1) ;
-    double s1=omp_get_wtime();
-    smooth(x,y,n,a,b,c);
-    double s2=omp_get_wtime();
-    double t4= (s2 - s1) ;
-    double c1=omp_get_wtime();
+    smooth(x,y,n,a,b,c,myid,dim,numprocs);
     count(x,n,t,&ctx);
-    double c2=omp_get_wtime();
-    double t5= (c2 - c1);
-    double c3=omp_get_wtime();
     count(y,n,t,&cty);
-    double c4=omp_get_wtime();
-    double t6= (c4 - c3);
 
-
+/*
     printf("Summary:\n");
     printf("Number of elements in a row/column: %d\n",n);
     printf("Number of inner elements in a row/column: %d\n",n-2);
@@ -117,7 +139,7 @@ printf("This is thread %d\n", omp_get_thread_num());
     printf("CPU: Smooth: %f\n",t4);    
     printf("CPU: Count-X: %f\n",t5);    
     printf("CPU: Count-Y: %f\n",t6);    
-
+*/
     return 0;
 };
 
